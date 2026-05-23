441c9e3b7b4e260e7c6b68e2cd73c73356879667
Jake <jakeob.scaglione@gmail.com>
2026-05-23 06:07:24 -0600

    Updated to reflect port template

--- Cisco/Cisco_Spare_Port.sh
+++ Cisco/Cisco_Spare_Port.sh
@@ -6,14 +6,29 @@ USER="admin"
 PASSWORD="admin"
 
 # Begin SSH login string. All options are due to old code train on the 3850.   
-sshpass -p "$PASSWORD"	ssh $USER@$DEVICE << "EOF" \
+sshpass -p "$PASSWORD"	ssh $USER@$DEVICE << EOF \
 	-o kexalgorithms=diffie-hellman-group-exchange-sha1 \
 	-o hostkeyalgorithms=ssh-rsa \
 	-o MACs=hmac-sha1-96 \
 
+# Begin Cisco configuration and create shutdown vlan, spare port template, and apply to a range of interfaces.
 configure terminal
+
+vlan 999
+name SPARE
+shutdown
+exit
+
+template SPARE_PORT
+switchport mode access
+switchport access vlan 999
+spanning-tree guard root
+spanning-tree bpduguard enable
+spanning-tree portfast
+
 interface range GigabitEthernet1/0/1-47
 description SPARE
 source template SPARE_PORT
 shutdown
+
 EOF
